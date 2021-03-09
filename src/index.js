614a932255691bb8e24f18c6d407604d1f77d730
Андрей Поташин <PotashinAndrey@users.noreply.github.com>
2021-03-10 00:24:43 +0500

    for submit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || !array.length) {
+        return 0;
+    }
+
+  return array.sort((a, b) => a - b)[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || !array.length) {
+        return 0;
+    }
+
+  return array.sort((a, b) => a - b)[array.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || !array.length) {
+        return 0;
+    }
+
+  let sum = array.reduce((a, c) => {
+    return a + c;
+  }, 0);
+
+  return sum/array.length;
 }
